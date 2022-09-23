a4b5ceacf36cffe1612fe9f6dd66917d95bbc57b
Pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-09-23 10:34:22 -0300

    N6 - Nodemon and Creating the Server with Expres

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
 // Criando um Servidor Web com Node.JS
 
-const http = require('http');
+const express = require('express');
 
-let server = http.createServer((req, res) => {
-
-    console.log('URL:', req.url);
-    console.log('METHOD:', req.method);
-
-    switch (req.url) {
-
-        case '/':
-
-            res.statusCode = 200;
-            res.setHeader('Content-type', 'text/html');
-            res.end('<h1>Olá</h1>');
-
-        break;
-
-        case '/users':
-            res.statusCode = 200;
-            res.setHeader('Content-type', 'application/json');
-            res.end(JSON.stringify)({
-                users:[{
-                    name:'Hcode',
-                    email:'contato@hcode.com.br',
-                    id:1
-                }]
-            });
-            res.end('<h1>Olá</h1>');
-
-            break;
-    }
+let app = express();
 
 
-
+app.get('/', (req, res) =>{
 
     
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/html');
+    res.end('<h1>Olá</h1>');
 });
 
-server.listen(3000, '127.0.0.1', () => {
+
+app.get('/users', (req, res) => {
+
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'application/json');
+    res.json({
+        users: [{
+            name: 'Hcode',
+            email: 'contato@hcode.com.br',
+            id: 1
+        }]
+    });
+
+});
+
+
+
+app.listen(3000, '127.0.0.1', () =>{
 
     console.log('servidor rodando!');
 });
