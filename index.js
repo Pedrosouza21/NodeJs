86b8b099c0fde7403029120626cf6b3094450b35
Pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-09-23 09:48:25 -0300

    N4 - Detecting different URL

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
+// Criando um Servidor Web com Node.JS
+
 const http = require('http');
 
-let server = http.createServer((req, res)=>{
+let server = http.createServer((req, res) => {
 
-    console,log('URL:',req.url );
+    console.log('URL:', req.url);
     console.log('METHOD:', req.method);
 
-    res.end('Ok');
+    switch (req.url) {
+
+        case '/':
+
+            res.statusCode = 200;
+            res.setHeader('Content-type', 'text/html');
+            res.end('<h1>Olá</h1>');
+
+        break;
+
+        case '/users':
+            res.statusCode = 200;
+            res.setHeader('Content-type', 'application/json');
+            res.end(JSON.stringify)({
+                users:[{
+                    name:'Hcode',
+                    email:'contato@hcode.com.br',
+                    id:1
+                }]
+            });
+            res.end('<h1>Olá</h1>');
+
+            break;
+    }
+
+
+
+
+    
 });
 
-server.listen(3000, '127.0.0.1',()=>{
+server.listen(3000, '127.0.0.1', () => {
 
     console.log('servidor rodando!');
 });
