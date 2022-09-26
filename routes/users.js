7f1205d062356972bbf9b3a84c221a76894fc7ae
Pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-09-26 08:55:00 -0300

    N14 - Editing a User

--- routes/users.js
+++ routes/users.js
@@ -68,5 +68,22 @@ module.exports = app =>{
 
     });
 
+    // editando dados de usuário
+    routeId.put((req, res) => {
+
+        db.update({_id: req.params.id }, req.body, err => {
+
+            if(err) {
+                app.utils.error.send(err,req, res);
+            
+            }else{
+                res.status(200).json(Object.assign(req.params, req.body));
+            }
+        
+        });
+
+
+    });
+
     
 };
