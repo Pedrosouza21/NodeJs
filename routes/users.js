7b71a5052fffc11ea949d20ed251bf50b3033fad
Pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-09-26 10:11:10 -0300

    N16 - Validating data received via Post

--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,52 @@
 let NeDB = require('nedb');
-let db =  new NeDB({
+let db = new NeDB({
     filename: 'users.db',
-    autoload:true
+    autoload: true
 });
 
 
-module.exports = app =>{
+module.exports = app => {
 
     let route = app.route('/users');
 
-    route.get((req, res)=>{
+    route.get((req, res) => {
 
-        db.find({}).sort({name:1}).exec((err, users)=>{
+        db.find({}).sort({ name: 1 }).exec((err, users) => {
 
-             if(err) {
+            if (err) {
                 app.utils.error.send(err, req, res);
 
-            }else {
+            } else {
                 res.statusCode = 200;
                 res.setHeader('Content-type', 'application/json');
                 res.json({
                     users
 
                 });
-                
+
             }
         });
-        
+
 
     });
-  
-    
-    route.post((req, res)=>{
 
-        db.insert(req.body, (err, user)=>{
 
-            if(err) {
-                app.utils.error.send(err,req, res);
-            
-            }else{
+    route.post((req, res) => {
+      if(!app.utils.validator.user(app, req, res)) return false;
+       
+
+        db.insert(req.body, (err, user) => {
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+
+            } else {
 
                 res.status(200).json(user);
             }
-        
+
         });
 
-        
-    
     });
 
     // procura o dados do usuário
@@ -54,15 +54,15 @@ module.exports = app =>{
 
     routeId.get((req, res) => {
 
-        db.findOne({_id:req.params.id}).exec((err, user)=>{
+        db.findOne({ _id:req.params.id }).exec((err, user) => {
 
-            if(err) {
-                app.utils.error.send(err,req, res);
-            
-            }else{
+            if (err) {
+                app.utils.error.send(err, req, res);
+
+            } else {
                 res.status(200).json(user);
             }
-        
+
         });
 
 
@@ -71,34 +71,36 @@ module.exports = app =>{
     // editando dados de usuário
     routeId.put((req, res) => {
 
-        db.update({_id: req.params.id }, req.body, err => {
+        if(!app.utils.validator.user(app, req, res)) return false;
 
-            if(err) {
-                app.utils.error.send(err,req, res);
-            
-            }else{
+        db.update({ _id: req.params.id }, req.body, err => {
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+
+            } else {
                 res.status(200).json(Object.assign(req.params, req.body));
             }
-        
+
         });
 
 
     });
 
 
-// excluindo um usuário
-    routeId.delete((req, res)=>{
+    // excluindo um usuário
+    routeId.delete((req, res) => {
 
-        db.remove({_id: req.params.id}, {}, err=>{
-            if(err) {
-                app.utils.error.send(err,req, res);
-            
-            }else{
+        db.remove({ _id: req.params.id }, {}, err => {
+            if (err) {
+                app.utils.error.send(err, req, res);
+
+            } else {
                 res.status(200).json(req.params);
             }
 
         });
     });
 
-    
+
 };
